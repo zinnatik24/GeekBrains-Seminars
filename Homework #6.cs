26b6e24934496d2162895b22c2260a4ba7920e94
Zinnat Maksutov <108758683+zinnatik24@users.noreply.github.com>
2022-08-24 17:55:01 +0500

    version1

---
+++ Homework #6.cs
@@ -0,0 +1,22 @@
+﻿// ZaDACHA 39
+// int size = Convert.ToInt32(Console.ReadLine());
+// int [] array = new int[size];
+
+// for (int i = 0; i < size; i++)
+// {
+//     array[i] = new Random().Next(11);
+// }
+
+// Console.WriteLine($" Array: [{String.Join("; ", array)} ]");
+
+// int [] result_array = new int[size];
+// int lastIndex = size - 1;
+// for (int i = 0; i < size; i++)
+// {
+//     result_array[i] = array[lastIndex - i];
+// }
+// Console.WriteLine($" Reverse Array: [{String.Join("; ", result_array)} ]");
+
+//----------------------------------------------------------------
+// another method to reserve with a help of methods
+
